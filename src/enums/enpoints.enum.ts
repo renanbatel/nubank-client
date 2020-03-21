b82d9e21a9f557e6c211d643e4a0aab2db466566
renanbatel <renanbatel@gmail.com>
2020-03-21 10:14:52 -0300

    chore: add discovery urls

---
+++ src/enums/enpoints.enum.ts
@@ -0,0 +1,4 @@
+export enum Endpoints {
+  API_DISCOVERY = 'https://prod-s0-webapp-proxy.nubank.com.br/api/discovery',
+  APP_DISCOVERY = 'https://prod-s0-webapp-proxy.nubank.com.br/api/app/discovery',
+}
---
+++ src/enums/index.ts
@@ -0,0 +1,1 @@
+export * from './enpoints.enum';
