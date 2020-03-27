68b802c49bfa2be075d76a6eeb65491a150a254f
renanbatel <renanbatel@gmail.com>
2020-03-26 22:35:21 -0300

    chore: main base files

--- src/enums/enpoints.enum.ts
+++
@@ -1,4 +0,0 @@
-export enum Endpoints {
-  API_DISCOVERY = 'https://prod-s0-webapp-proxy.nubank.com.br/api/discovery',
-  APP_DISCOVERY = 'https://prod-s0-webapp-proxy.nubank.com.br/api/app/discovery',
-}
