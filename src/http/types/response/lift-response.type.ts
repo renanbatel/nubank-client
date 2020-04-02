d159ccc4d9b46905a6697efa0ca23727bedfab18
renanbatel <renanbatel@gmail.com>
2020-04-02 00:36:40 -0300

    chore: set response and urls properties to required

--- src/http/types/response/lift-response.type.ts
+++ src/http/types/response/lift-response.type.ts
@@ -1,7 +1,7 @@
 import { FeaturesUrls } from '../urls';
 
 export type LiftResponse = {
-  access_token?: string;
-  token_type?: string;
-  _links?: FeaturesUrls;
+  access_token: string;
+  token_type: string;
+  _links: FeaturesUrls;
 };
